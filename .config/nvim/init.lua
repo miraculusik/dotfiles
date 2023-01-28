9110aeffcbd16652f0b93a42f4b7871dbfc3ba9d
Miraç ULUŞIK <miul@mac-A.local>
2023-01-28 18:59:03 +0300

    updated

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -507,21 +507,21 @@ null_ls.setup({
 	end,
 })
 
--- TODO: emmet_ls
-local lspconfig = require('lspconfig')
-local capabilitiesE = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-lspconfig.emmet_ls.setup({
-	-- on_attach = on_attach,
-	capabilities = capabilitiesE,
-	filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
-	init_options = {
-		html = {
-			options = {
-				-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-				["bem.enabled"] = true,
-			},
-		},
-	}
-})
+-- -- TODO: emmet_ls
+-- local lspconfig = require('lspconfig')
+-- local capabilitiesE = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.completion.completionItem.snippetSupport = true
+--
+-- lspconfig.emmet_ls.setup({
+-- 	-- on_attach = on_attach,
+-- 	capabilities = capabilitiesE,
+-- 	filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+-- 	init_options = {
+-- 		html = {
+-- 			options = {
+-- 				-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+-- 				["bem.enabled"] = true,
+-- 			},
+-- 		},
+-- 	}
+-- })
